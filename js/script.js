24448f125f00a3274dc1e2140d16832ef28e6b8b
tessyGrace <112434170+tessyGrace@users.noreply.github.com>
2022-08-29 21:57:47 +0100

    Add Initial cv website files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//===================================== Typing Animation ====================================
+
+// var typed = new typed(".typing",{
+//     string:["Web designer","web developer","Anroide developer","YouTuber"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+
+// })
+
+var typed = new Typed(".typing",{
+
+    strings:["", "Web Designer","", "Web Developer", "", "YouTuber","","Mobile Designer"],
+    typespeed:300,
+    Backspeed:100,
+    loop:true
+})
